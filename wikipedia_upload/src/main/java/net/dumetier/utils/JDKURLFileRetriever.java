d96609b17aac0342adae970c2f21be57d89a5be0
Julien DUMETIER <jdumetier@generixgroup.com>
2014-03-14 17:28:40 +0100

    Correction des downloads

--- wikipedia_upload/src/main/java/net/dumetier/utils/JDKURLFileRetriever.java
+++ wikipedia_upload/src/main/java/net/dumetier/utils/JDKURLFileRetriever.java
@@ -1,8 +1,10 @@
 package net.dumetier.utils;
 
 import java.io.BufferedInputStream;
+import java.io.Closeable;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,26 +13,41 @@ import net.dumetier.wikipedia.FileInfo;
 
 public class JDKURLFileRetriever {
 
-  private static Logger logger = Logger.getLogger(JDKURLFileRetriever.class
-      .getCanonicalName());
+	private static Logger logger = Logger.getLogger(JDKURLFileRetriever.class.getCanonicalName());
 
-  public FileInfo retrieve(URL url) throws FileNotFoundException {
+	public FileInfo retrieve(URL url) throws FileNotFoundException {
 
-    BufferedInputStream in = null;
-    int contentLength = -1;
-    try {
-      contentLength = url.openConnection().getContentLength();
-      in = new BufferedInputStream(url.openStream());
-    } catch (IOException e) {
-      if (logger.isLoggable(Level.FINE)) {
-        logger.fine(String.format(
-            "Une erreur est survenue lors de la récupération du fichier %s.",
-            url.toExternalForm())
-            + "; " + e.getMessage());
-      }
-      throw new FileNotFoundException(String.format(
-          "Impossible de récupérer le fichier %s.", url.toExternalForm()));
-    }
-    return new FileInfo(in, contentLength);
-  }
+		BufferedInputStream in = null;
+		int contentLength = -1;
+		HttpURLConnection conn = null;
+		try {
+			conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			contentLength = conn.getContentLength();
+
+			in = new BufferedInputStream(url.openStream());
+		} catch (IOException e) {
+			if (logger.isLoggable(Level.FINE)) {
+				logger.fine(String.format("Une erreur est survenue lors de la récupération du fichier %s.", url.toExternalForm()) + "; "
+						+ e.getMessage());
+			}
+			throw new FileNotFoundException(String.format("Impossible de récupérer le fichier %s.", url.toExternalForm()));
+		}
+		return new FileInfo(in, contentLength, new ConnectionWrapper(conn));
+	}
+
+	private static class ConnectionWrapper implements Closeable {
+
+		private final HttpURLConnection conn;
+
+		public ConnectionWrapper(HttpURLConnection conn) {
+			this.conn = conn;
+		}
+
+		@Override
+		public void close() throws IOException {
+			conn.disconnect();
+		}
+
+	}
 }
--- wikipedia_upload/src/main/java/net/dumetier/wikipedia/WikipediaArchiveDownloader.java
+++ wikipedia_upload/src/main/java/net/dumetier/wikipedia/WikipediaArchiveDownloader.java
@@ -2,6 +2,7 @@ package net.dumetier.wikipedia;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.Date;
@@ -13,90 +14,85 @@ import net.dumetier.utils.JDKURLFileRetriever;
 
 public class WikipediaArchiveDownloader {
 
-  private static final Logger logger = Logger
-      .getLogger(WikipediaArchiveDownloader.class.getCanonicalName());
-  public static final String FILENAME_PATTERN = "pagecounts-%1$tY%1$tm%1$td-%1$tH00%1$tS.gz";
+	private static final Logger logger = Logger.getLogger(WikipediaArchiveDownloader.class.getCanonicalName());
+	public static final String FILENAME_PATTERN = "pagecounts-%1$tY%1$tm%1$td-%1$tH00%1$tS.gz";
 
-  private final File outputDirectory;
+	private final File outputDirectory;
 
-  public WikipediaArchiveDownloader(String outputDirectory) {
+	public WikipediaArchiveDownloader(String outputDirectory) {
 
-    this.outputDirectory = new File(outputDirectory);
-    if (!this.outputDirectory.isDirectory()) {
-      throw new RuntimeException("The outputdirectory is not a directory");
-    }
-  }
+		this.outputDirectory = new File(outputDirectory);
+		if (!this.outputDirectory.isDirectory()) {
+			throw new RuntimeException("The outputdirectory is not a directory");
+		}
+	}
 
-  public List<URL> getAlreadyDownloadedFile() {
-    return new ArrayList<URL>();
-  }
+	public List<URL> getAlreadyDownloadedFile() {
+		return new ArrayList<URL>();
+	}
 
-  public List<URL> getMissingFiles() {
-    return new ArrayList<URL>();
-  }
+	public List<URL> getMissingFiles() {
+		return new ArrayList<URL>();
+	}
 
-  public void downloadAllBetween(Date from, Date to) {
-    long resteFrom = from.getTime() % (60L * 60L * 1000L);
-    long resteTo = to.getTime() % (60L * 60L * 1000L);
-    from = new Date(from.getTime() - resteFrom);
-    to = new Date(to.getTime() - resteTo);
+	public void downloadAllBetween(Date from, Date to) {
+		long resteFrom = from.getTime() % (60L * 60L * 1000L);
+		long resteTo = to.getTime() % (60L * 60L * 1000L);
+		from = new Date(from.getTime() - resteFrom);
+		to = new Date(to.getTime() - resteTo);
 
-    System.out.println("from:" + from);
-    System.out.println("to:" + to);
+		System.out.println("from:" + from);
+		System.out.println("to:" + to);
 
-    WikipediaArchiveURLCreator urlCreator = new WikipediaArchiveURLCreator();
-    for (Date current = from; current.getTime() < to.getTime(); current = new Date(
-        current.getTime() + (60 * 60 * 1000))) {
-      System.out.println(current);
-      try {
-        doNothing();
-        doDownloadAndStore(current, urlCreator);
-      } catch (FileNotFoundException e) {
-        if (logger.isLoggable(Level.FINE)) {
-          logger.fine("Le fichier n'a pas été trouvé : " + e.getMessage());
-        }
-        // Parfois l'horodatage est décalé d'une seconde
-        try {
-          doDownloadAndStore(new Date(current.getTime() + (1 * 1000)),
-              urlCreator);
-        } catch (FileNotFoundException e2) {
-          if (logger.isLoggable(Level.FINE)) {
-            logger
-                .fine("Le fichier décallé d'une seconde n'a pas été trouvé : "
-                    + e2.getMessage());
-          }
-        }
-      }
-    }
-  }
+		WikipediaArchiveURLCreator urlCreator = new WikipediaArchiveURLCreator();
+		for (Date current = from; current.getTime() < to.getTime(); current = new Date(current.getTime() + (60 * 60 * 1000))) {
+			System.out.println(current);
+			try {
+				doNothing();
+				doDownloadAndStore(current, urlCreator);
+			} catch (IOException e) {
+				if (logger.isLoggable(Level.FINE)) {
+					logger.fine("Le fichier n'a pas été trouvé : " + e.getMessage());
+				}
+				// Parfois l'horodatage est décalé d'une seconde
+				try {
+					doDownloadAndStore(new Date(current.getTime() + (1 * 1000)), urlCreator);
+				} catch (IOException e2) {
+					if (logger.isLoggable(Level.FINE)) {
+						logger.fine("Le fichier décallé d'une seconde n'a pas été trouvé : " + e2.getMessage());
+					}
+				}
+			}
+		}
+	}
 
-  private void doNothing() throws FileNotFoundException {
-  }
+	private void doNothing() throws FileNotFoundException {
+	}
 
-  private void doDownloadAndStore(Date current,
-      WikipediaArchiveURLCreator urlCreator) throws FileNotFoundException {
-    URL url = urlCreator.createURL(current);
-    String fileName = String.format(FILENAME_PATTERN, current);
-    downloadAndStoreAs(url, fileName);
+	private void doDownloadAndStore(Date current, WikipediaArchiveURLCreator urlCreator) throws IOException {
+		URL url = urlCreator.createURL(current);
+		String fileName = String.format(FILENAME_PATTERN, current);
+		downloadAndStoreAs(url, fileName);
 
-  }
+	}
 
-  public void downloadAndStoreAs(URL url, String fileName)
-      throws FileNotFoundException {
-    FileInfo fileInfo = new JDKURLFileRetriever().retrieve(url);
-    if (logger.isLoggable(Level.FINER)) {
-      logger.finer("Taille fichier en octets : " + fileInfo.getContentLength()
-          + " ; en Mo" + fileInfo.getContentLength() / (1024L * 1024));
-    }
-    long byteCopied = new CopyStreamToDisk(outputDirectory).processInputStream(
-        fileInfo.getInputStream(), fileName);
-    if (logger.isLoggable(Level.FINER)) {
-      logger.finer("Copie complete ?  "
-          + (fileInfo.getContentLength() == byteCopied));
-      logger.finer("Taille sauvegardée en octets : "
-          + fileInfo.getContentLength() + " ; en Mo"
-          + fileInfo.getContentLength() / (1024L * 1024));
-    }
+	public void downloadAndStoreAs(URL url, String fileName) throws IOException {
+		FileInfo fileInfo = new JDKURLFileRetriever().retrieve(url);
+		if (logger.isLoggable(Level.FINER)) {
+			logger.finer("Taille fichier en octets : " + fileInfo.getContentLength() + " ; en Mo" + fileInfo.getContentLength()
+					/ (1024L * 1024));
+		}
+		long byteCopied = -1;
+		try {
+			byteCopied = new CopyStreamToDisk(outputDirectory).processInputStream(fileInfo.getInputStream(), fileName);
+		} finally {
+			fileInfo.getConnection().close();
+		}
+		if (logger.isLoggable(Level.FINER)) {
+			logger.finer("Copie complete ?  " + (fileInfo.getContentLength() == byteCopied));
+			logger.finer("Taille sauvegardée en octets : " + fileInfo.getContentLength() + " ; en Mo" + fileInfo.getContentLength()
+					/ (1024L * 1024));
+		}
 
-  }
+	}
 }
